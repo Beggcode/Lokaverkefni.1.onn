d079080e27d353e235cf6a554398be6ac370c594
Beggcode <Beggman95@gmail.com>
2025-12-10 20:14:49 +0000

    Byrjaði að setja upp verkefnið og rannska/hugsa hvað ég vill gera

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useState } from 'react'
+import {  BrowserRouter, Route } from 'react-router-dom'
+
+function App() {
+    
+
+  return (
+    
+   
+  )
+}
+
+export default App
